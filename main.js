d28e5953924da2f3295a2c3ad8954803dfb787ed
Bruno Franco <me.brunoo@gmail.com>
2015-03-14 15:58:49 +0000

    reset button

--- main.js
+++ main.js
@@ -1,44 +1,50 @@
-var MINUTE = 60*1000
-var AUTO_CLOSE_NOTIF_TIME = 5*MINUTE
-var START = 1
-var STATUS = 2
-var RESET = 3
 var SECOND = 1000
+var MINUTE = 60*SECOND
+var AUTO_CLOSE_NOTIF_TIME = 0.1*MINUTE
+var START = 1
+var RESET = 2
+var CONTINUE = 3
 var timeLeft = 0
 
 // Start the counter
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	if (request.command == START) {
-  		timeLeft = request.time
-  		if (request.time == 0) {
-  			notification()
-  		} else {
-  			setTimeout(function() { sendMsg(request); }, SECOND)
-  		}
-  	}
-
-  	if (request.command == STATUS) {
-  		sendResponse({command: STATUS, time: timeLeft})
+  	switch (request.command) {
+  		case CONTINUE:
+  			if (timeLeft == 0) {
+					notification("Time to take a break!")
+          sendContMsg({command: CONTINUE, time: 0})
+          timeLeft = -1
+          break;
+        }
+        if (timeLeft < 0) { break;}
+      case START:
+        setTimeout(function() { sendContMsg({command: CONTINUE, time: request.time - SECOND}) }, SECOND)
+        timeLeft = request.time-SECOND
+        break;
+  		case RESET:
+  			timeLeft = -1
+  			sendResponse({command: RESET})
+  			break;
   	}
 });
 
 // Send message each second
 // Needed to keep track of the seconds left
-function sendMsg (request) {
-	chrome.runtime.sendMessage({command: START, time: request.time-SECOND});
+function sendContMsg (message) {
+	chrome.runtime.sendMessage(message);
 }
 
 // Creates the notification
-// Autoclose notification after 5 minutes.
-function notification () {
+function notification (message) {
 	var path = chrome.runtime.getURL("/tomato.png")
 	var notifId = Math.random().toString()
 	chrome.notifications.create(
-		  notifId,{   
+		  notifId,
+      {   
 		      type: "basic", 
 		      title: "Time Warn", 
-		      message: "Time to take a break!",
+		      message: message,
 		      iconUrl: path
 		  },
 		  function() {
--- popup.js
+++ popup.js
@@ -2,13 +2,13 @@ var SECOND = 1000
 var MINUTE = 60*SECOND
 var AUTO_CLOSE_NOTIF_TIME = 5*MINUTE
 var DEFAULT_TIME = 20
+var TEN_SEC = 10
 var START = 1
-var STATUS = 2
-var RESET = 3
+var RESET = 2
+var CONTINUE = 3
 
 // add the current number of minutes
 function show_minutes() {
-
   // Use default value, 20.
   chrome.storage.sync.get({
     minutes: DEFAULT_TIME,
@@ -22,20 +22,49 @@ function show_minutes() {
 
 // resets the timer
 function reset () {
-	document.getElementById('minutes').innerHTML = ''
-	sendMsg({command: RESET})
+	sendMsg({command: RESET}, function(response) { clear_timer() } )
 }
 
-// Starts the timer.
-function start_clock () {
+// clear the timer display
+function clear_timer () {
+  document.getElementById('timeLeft').innerHTML = ''
+}
+
+// starts the timer
+function start_timer () {
 	var minutes = document.getElementById('minutes').value
 	sendMsg({command: START, time: minutes*MINUTE})
 }
 
-function sendMsg (message) {
-	chrome.runtime.sendMessage(message);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    switch (request.command) {
+      case CONTINUE:
+        var min = request.time > MINUTE ? Math.floor(request.time / SECOND) : 0
+        var sec = 0
+        if (request.time < MINUTE) {
+          if ((request.time / SECOND) >= TEN_SEC) {
+            sec = request.time / SECOND
+          } else {
+            sec = '0' + request.time / SECOND
+          }
+        } else {
+          sec = request.time % SECOND
+        }
+        show_timer(min, sec)
+        break;
+    }
+});
+
+function show_timer (min, sec) {
+  document.getElementById('timeLeft').innerHTML = min + ":" + sec
+}
+
+// send message to component
+function sendMsg (message, callback) {
+  chrome.runtime.sendMessage(message, callback);
 }
 
 document.addEventListener('DOMContentLoaded', show_minutes)
 document.getElementById("resetBtn").addEventListener("click", reset)
-document.getElementById("startBtn").addEventListener("click", start_clock)
+document.getElementById("startBtn").addEventListener("click", start_timer)
